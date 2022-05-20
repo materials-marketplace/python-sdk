a7ec718589eecc919a7c64743c82c92db7280a9c
Carl Simon Adorf <simon.adorf@epfl.ch>
2022-05-20 11:21:43 +0200

    Allow instantiation of app resource from string.

--- marketplace/app/base.py
+++ marketplace/app/base.py
@@ -1,4 +1,5 @@
 from urllib.parse import urljoin
+from uuid import UUID
 
 from ..applicaton_proxy import ApplicationProxy
 from ..client import Client
@@ -8,10 +9,14 @@ from ..models import ApplicationId
 class _AppBase:
     """Interact with a MarketPlace application."""
 
-    def __init__(self, application_id: ApplicationId, **kwargs):
+    def __init__(self, application_id: ApplicationId | str, **kwargs):
         self.client = Client(**kwargs)
-        self.application_id = application_id
-        self.application_proxy = ApplicationProxy(self.client, application_id)
+        self.application_id = (
+            ApplicationId(UUID(application_id))
+            if isinstance(application_id, str)
+            else application_id
+        )
+        self.application_proxy = ApplicationProxy(self.client, self.application_id)
 
         self._capabilities = self._determine_capabilities
 

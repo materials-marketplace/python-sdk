2d7356d37ccbb72ed735aa1e1a643c7c9266df9c
Yoav Nahshon <yoav.nahshon@iwm.fraunhofer.de>
2022-05-27 21:28:25 +0200

    Restore headers in models

--- marketplace/message_broker/models/response_message.py
+++ marketplace/message_broker/models/response_message.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any, Dict, Optional
 
 from pydantic import BaseModel, Field
 
@@ -7,3 +7,4 @@ class ResponseMessage(BaseModel):
     status_code: int = Field(200, description="The HTTP response code")
     body_base64: str = Field(None, description="A base64-encrypted body")
     body: Optional[Any] = Field(None, description="The response message payload")
+    headers: Optional[Dict[str, str]] = Field(None, description="Response headers")

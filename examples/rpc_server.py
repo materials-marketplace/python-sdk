5e4f18ae206b8a17db2345e232b3bba6410f0dc2
nash <62147380+yoavnash@users.noreply.github.com>
2022-06-02 13:03:39 +0200

    Add message broker module (#11)

---
+++ examples/rpc_server.py
@@ -0,0 +1,33 @@
+import json
+
+from marketplace_standard_app_api.models.message_broker import (
+    MessageBrokerRequestModel,
+    MessageBrokerResponseModel,
+)
+
+from marketplace.message_broker.rpc_server import RpcServer
+
+
+def my_message_handler(
+    request_message: MessageBrokerRequestModel,
+) -> MessageBrokerResponseModel:
+    print("Routing to endpoint %r..." % request_message.endpoint)
+    payload = json.loads(request_message.body) if request_message.body else {}
+    result = len(payload)
+    response = {"numberOfKeysInPayload": str(result)}
+    print("Done!")
+    response_message = MessageBrokerResponseModel(
+        status_code=200,
+        body=json.dumps(response),
+        headers={"Content-Type": "application/json"},
+    )
+    return response_message
+
+
+rpc_server = RpcServer(
+    host="www.materials-marketplace.eu",
+    application_id="<application-id>",
+    application_secret="<application-secret>",
+    message_handler=my_message_handler,
+)
+rpc_server.consume_messages()

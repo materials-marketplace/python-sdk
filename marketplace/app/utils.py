0e1348c9bde073f36f8ef6bea3208df17ce0f2a2
Jusong Yu <jusong.yeu@gmail.com>
2022-06-29 14:15:10 +0200

    Update check_capability_availability check with argument

--- marketplace/app/utils.py
+++ marketplace/app/utils.py
@@ -5,23 +5,27 @@
 """
 
 import re
-from functools import wraps
+import functools
 
 
-def check_capability_availability(capability):
+def check_capability_availability(capability=None):
     """Decorator for checking that a certain app supports a given capability.
 
     Args:
         capability (str): capability that should be in capabilities
     """
+    def decorator_check(func):
+        @functools.wraps(func)
+        def wrapper(instance, *args, **kwargs):
+            if not capability:
+                capability = func.__name__
+                
+            if capability not in instance.capabilities:
+                raise NotImplementedError("The app does not support this capability.")
+            return func(instance, *args, **kwargs)
 
-    @wraps(capability)
-    def wrapper(instance, *args, **kwargs):
-        if capability.__name__ not in instance.capabilities:
-            raise NotImplementedError("The app does not support this capability.")
-        return capability(instance, *args, **kwargs)
-
-    return wrapper
+        return wrapper
+    return decorator_check
 
 
 def camel_to_snake(name):

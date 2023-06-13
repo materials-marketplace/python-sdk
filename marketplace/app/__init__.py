31ba928047fc01ff0c43c8648f09da102e165fa8
Pablo de Andres <9430234+pablo-de-andres@users.noreply.github.com>
2023-06-13 11:12:55 +0200

    Remove v0.0.1 support (#63)

--- marketplace/app/__init__.py
+++ marketplace/app/__init__.py
@@ -3,7 +3,6 @@ capabilities.
 .. currentmodule:: marketplace.app
 .. moduleauthor:: Pablo de Andres, Pranjali Singh (Fraunhofer IWM)
 """
-import warnings
 from typing import Optional
 
 from packaging.version import parse
@@ -11,16 +10,6 @@ from packaging.version import parse
 from ..client import MarketPlaceClient
 from .utils import camel_to_snake
 from .v0 import MarketPlaceApp as _MarketPlaceApp_v0
-from .v0_0_1 import MarketPlaceApp as _MarketPlaceApp_v0_0_1
-
-
-class MarketPlaceApp(_MarketPlaceApp_v0_0_1):
-    def __init__(self, *args, **kwargs):
-        warnings.warn(
-            "The MarketPlaceApp class is deprecated as of v0.2.0 and will be "
-            "removed in v1. Please use the get_app() function instead."
-        )
-        super().__init__(*args, **kwargs)
 
 
 def get_app(app_id, client: Optional[MarketPlaceClient] = None, **kwargs):
@@ -36,25 +25,13 @@ def get_app(app_id, client: Optional[MarketPlaceClient] = None, **kwargs):
     # Getting api version and list of capabilities for the application
     app_service_path = f"api/applications/{app_id}"
     app_info: dict = client.get(path=app_service_path).json()
-    app_api_version = parse(app_info.get("api_version", "0.0.1"))
+    app_api_version = parse(app_info.get("api_version", "1.0.0"))
 
     capabilities = []
     for capability in app_info["capabilities"]:
         capabilities.append(camel_to_snake(capability["name"]))
 
-    if app_api_version == parse("0.0.1"):
-        if client is not None:
-            raise RuntimeError(
-                "Cannot use existing client for apps with API version 0.0.1."
-            )
-        return _MarketPlaceApp_v0_0_1(
-            app_id,
-            marketplace_host_url=kwargs.get("marketplace_host_url"),
-            access_token=kwargs.get("access_token"),
-            capabilities=capabilities,
-            **kwargs,
-        )
-    elif parse("0.0.1") < app_api_version <= parse("0.3.0"):
+    if app_api_version < parse("1.0.0"):
         return _MarketPlaceApp_v0(app_id, app_info, client, **kwargs)
     else:
         raise RuntimeError(f"App API version ({app_api_version}) not supported.")

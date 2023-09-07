8dd7267ebda1a214ce9b600de8b921ce918eb63a
Kiran Kumaraswamy <kiran.kumaraswamy@iwm.fraunhofer.de>
2023-09-07 15:36:51 +0000

    inherit missing system class in marketplace app

--- marketplace/app/v0/__init__.py
+++ marketplace/app/v0/__init__.py
@@ -1,6 +1,9 @@
 from .object_storage import MarketPlaceObjectStorageApp
+from .system import MarketPlaceSystemApp
 from .transformation import MarketPlaceTransformationApp
 
 
-class MarketPlaceApp(MarketPlaceObjectStorageApp, MarketPlaceTransformationApp):
+class MarketPlaceApp(
+    MarketPlaceObjectStorageApp, MarketPlaceTransformationApp, MarketPlaceSystemApp
+):
     pass

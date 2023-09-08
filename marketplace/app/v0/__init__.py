f1d6efea88e372d59466a9b05a42418ba9f097a7
Kiran Kumaraswamy <kiran.kumaraswamy@iwm.fraunhofer.de>
2023-09-08 09:12:31 +0000

    remove the system import

--- marketplace/app/v0/__init__.py
+++ marketplace/app/v0/__init__.py
@@ -1,9 +1,6 @@
 from .object_storage import MarketPlaceObjectStorageApp
-from .system import MarketPlaceSystemApp
 from .transformation import MarketPlaceTransformationApp
 
 
-class MarketPlaceApp(
-    MarketPlaceObjectStorageApp, MarketPlaceTransformationApp, MarketPlaceSystemApp
-):
+class MarketPlaceApp(MarketPlaceObjectStorageApp, MarketPlaceTransformationApp):
     pass
